d3548f394f001f1300ab00c8926d78ebf6037c16
MARVIN HEGI <MARVIN_HEGI@sluz.ch>
2025-05-16 09:25:29 +0200

    decleard foods as object

--- src/tanzverbot-diet.ts
+++ src/tanzverbot-diet.ts
@@ -3,23 +3,24 @@ export enum Sex {
   Female = "f",
 }
 
-//ToDo in objekt
-const foodNames: string[] = [
-  "Kellogg's Tresor",
-  "Weihenstephan Haltbare Milch",
-  "Mühle Frikadellen",
-  "Volvic Tee",
-  "Neuburger lockerer Sahnepudding",
-  "Lagnese Viennetta",
-  "Schöller 10ForTwo",
-  "Ristorante Pizza Salame",
-  "Schweppes Ginger Ale",
-  "Mini Babybel",
-];
+type Food = {
+  name: string;
+  caloriesPerServing: number;
+  servings: number;
+};
 
-//ToDo
-const foodCalories: number[] = [137, 64, 271, 40, 297, 125, 482, 835, 37, 59];
-const foodServings: number[] = [4, 8, 4, 12, 1, 6, 2, 2, 25, 20];
+const foods: Food[] = [
+  { name: "Kellogg's Tresor", caloriesPerServing: 137, servings: 4 },
+  { name: "Weihenstephan Haltbare Milch", caloriesPerServing: 64, servings: 8 },
+  { name: "Mühle Frikadellen", caloriesPerServing: 271, servings: 4 },
+  { name: "Volvic Tee", caloriesPerServing: 40, servings: 12 },
+  { name: "Neuburger lockerer Sahnepudding", caloriesPerServing: 297, servings: 1 },
+  { name: "Lagnese Viennetta", caloriesPerServing: 125, servings: 6 },
+  { name: "Schöller 10ForTwo", caloriesPerServing: 482, servings: 2 },
+  { name: "Ristorante Pizza Salame", caloriesPerServing: 835, servings: 2 },
+  { name: "Schweppes Ginger Ale", caloriesPerServing: 37, servings: 25 },
+  { name: "Mini Babybel", caloriesPerServing: 59, servings: 20 },
+];
 
 export function calcDateOnDiet(
   currentWeightKg: number,
@@ -28,6 +29,9 @@ export function calcDateOnDiet(
   ageY: number,
   sex: Sex,
 ): number {
+
+
+  
   const weightGainKg = targetWeightKg - currentWeightKg;
   if (weightGainKg < 0) {
     throw new Error(`This diet is for gaining weight, not loosing it!`);
@@ -36,12 +40,11 @@ export function calcDateOnDiet(
     throw new Error(`You do not qualify for this kind of diet.`);
   }
   let dailyCaloriesOnDiet = 0;
-  //ToDo for Each
-  for (const index in foodNames) {
-    const calories = foodCalories[index] || 0;
-    const servings = foodServings[index] || 0;
-    dailyCaloriesOnDiet += calories * servings;
-  }
+ 
+
+foods.forEach(food => {
+  dailyCaloriesOnDiet += food.caloriesPerServing * food.servings;
+});
 
   //ToDo eigene function
   let dailyCaloriesBasicMetabolicRate = 0;
@@ -63,3 +66,4 @@ export function calcDateOnDiet(
   }
   return Math.ceil((9000 * weightGainKg) / dailyExcessCalories);
 }
+
